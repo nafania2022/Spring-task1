16c8cf7a28fd37a20ca49b21b6e97b491b8d9929
Nafania2022 <nafania1522@gmail.com>
2023-09-10 16:47:35 +0300

    update 0.0.1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,16 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        System.out.println(bean1.getMessage());
+        System.out.println(bean.equals(bean1));
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat1.equals(cat2));
+        System.out.println(cat1.getMessage());
+        System.out.println(cat2.getMessage());
+
     }
 }
